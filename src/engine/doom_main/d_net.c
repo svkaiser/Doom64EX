c32c489c7592de74c6e972013a9cd4187e60365f
Zohar Malamant <dotfloat@gmail.com>
2016-09-11 22:53:50 +0200

    engine: re-enable multiplayer
    Why did I disable it in the first place?

--- src/engine/doom_main/d_net.c
+++ src/engine/doom_main/d_net.c
@@ -261,12 +261,6 @@ static void D_NetWait(void) {
 
     I_Printf("---------------------------------------------\n\n");
 
-#ifndef USESYSCONSOLE
-	//I_NetWaitScreen();
-	I_Printf("Network play is currently disabled.\n");
-	I_Quit();
-#endif
-
     while(net_waiting_for_start) {
         CheckMD5Sums();
 
