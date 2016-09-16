45c3dacc7c0ce4e64e50b251c8dc3a6f78334bea
Zohar Malamant <dotfloat@gmail.com>
2016-09-16 14:57:55 +0200

    build: Archive packages after they've been created

--- build_scripts/nightly_build.rb
+++ build_scripts/nightly_build.rb
@@ -20,7 +20,8 @@ targets =
      os: 'win32',
      toolchain: 'toolchain-mingw32.cmake',
      binary: 'doom64ex.exe',
-     extern: 'https://dl.dropboxusercontent.com/u/7122698/extern-win32.zip' },
+     extern: 'https://dl.dropboxusercontent.com/u/7122698/extern-win32.zip',
+     extern_copy_libs: 'bin/*.dll' },
    { compiler: 'gcc',
      os: 'linux32',
      binary: 'doom64ex',
@@ -31,6 +32,7 @@ targets =
    { compiler: 'gcc',
      os: 'linux64',
      binary: 'doom64ex',
+     kexwad: true,
      extern: 'https://dl.dropboxusercontent.com/u/7122698/linux-x86_64.tar.gz',
      extern_copy_libs: 'lib/*.so',
      defines: { CMAKE_SKIP_RPATH: 'ON' } }]
@@ -42,10 +44,11 @@ install =
    { archive: 'doom64ex-linux.tar.gz',
      build: { linux32: '/linux32', linux64: '/linux64' },
      kexwad: '/data',
-     bootstrap: '/start.sh' }]
+     distrib: { 'linux_start.sh' => '/doom64ex.sh' } }]
 
 class Hash
   attr_accessor :logfile
+  attr_accessor :built
 
   def system(args)
     logfile.puts(%x[ #{args} ])
@@ -56,8 +59,8 @@ end
 rm_rf ROOT_DIR
 mkdir_p ROOT_DIR
 
-# puts "Cloning repo #{REPO}"
-# git = Git.clone(REPO, 'source', path: ROOT_DIR, depth: 1)
+puts "Cloning repo #{REPO}"
+git = Git.clone(REPO, 'source', path: ROOT_DIR, depth: 1)
 
 def build(target)
   root = "#{ROOT_DIR}/#{target[:os]}-#{target[:compiler]}"
@@ -69,8 +72,7 @@ def build(target)
   mkdir_p package
   target.logfile = File.new("#{root}/build.log", 'w')
 
-  # cp_r "#{ROOT_DIR}/source", "#{root}"
-  cp_r '/home/zohar/Projects/Doom64EX/.', source
+  cp_r "#{ROOT_DIR}/source", "#{root}"
 
   if target[:extern]
     if target[:extern].end_with? '.zip'
@@ -90,6 +92,13 @@ def build(target)
 
   cp_r Dir.glob("#{source}/extern/#{target[:extern_copy_libs]}"), package if target[:extern_copy_libs]
   cp "#{build}/src/engine/#{target[:binary]}", package
+
+  if target.fetch(:kexwad, false)
+    target.system("cmake --build #{build} --target kexwad --config Release")
+    cp "#{build}/kex.wad", "#{ROOT_DIR}/kex.wad"
+  end
+
+  target.built = true
 end
 
 threads = []
@@ -98,3 +107,35 @@ targets.each do |t|
   threads << Thread.new { build(t) }
 end
 threads.each { |t| t.join }
+
+install.each do |i|
+  oslist = i[:build].keys.map { |x| x.to_s }
+  ts = targets.select { |t| oslist.include? t[:os] }
+  next unless ts.all? { |t| t.built }
+
+  any_root = "#{ROOT_DIR}/#{ts[0][:os]}-#{ts[0][:compiler]}"
+  root = "#{ROOT_DIR}/install-#{i[:archive]}"
+
+  mkdir_p root
+  i[:build].each do |os,dir|
+    mkdir_p "#{root}/#{dir}"
+    t = ts.select { |tr| tr[:os] == os.to_s }[0]
+    cp_r "#{ROOT_DIR}/#{t[:os]}-#{t[:compiler]}/package/.", "#{root}/#{dir}"
+  end
+
+  (i[:distrib] || {}).each do |src,dest|
+    p "#{any_root}/source/distrib/#{src}"
+    p dest
+    cp "#{any_root}/source/distrib/#{src}", "#{root}/#{dest}"
+  end
+
+  mkdir_p "#{root}/#{i[:kexwad]}"
+  cp "#{ROOT_DIR}/kex.wad", "#{root}/#{i[:kexwad]}"
+
+  cd root
+  if i[:archive].end_with? '.zip'
+    system("zip -r #{ROOT_DIR}/#{i[:archive]} *")
+  elsif i[:archive].end_with? '.tar.gz'
+    system("tar czf #{ROOT_DIR}/#{i[:archive]} *")
+  end
+end
