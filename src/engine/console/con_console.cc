f56de08dd1786220bd3a0e5fe5535c7879426f80
Zohar Malamant <dotfloat@gmail.com>
2017-04-14 16:27:56 +0200

    Un-revert "Add optional GTK+3 windows"
    This reverts commit b854c2408a82e09726fb59e7beab45b80d886bf7.

--- src/engine/console/con_console.cc
+++ src/engine/console/con_console.cc
@@ -36,6 +36,7 @@
 #include "r_main.h"
 #include "i_system.h"
 #include "gl_texture.h"
+#include <imp/NativeUI>
 
 #define CONSOLE_PROMPTCHAR      '>'
 #define MAX_CONSOLE_LINES       256//must be power of 2
@@ -222,6 +223,8 @@ void CON_AddText(const char *text) {
 
         c = *(src++);
     }
+
+    native_ui::console_add_line(text);
 }
 
 //
